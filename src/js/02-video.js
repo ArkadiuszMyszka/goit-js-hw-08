35b030554a760c4f61af886f9a0b82a9b1dea71d
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-05-30 22:20:29 +0200

    Fix implemented

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('vimeo-player');
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 function saveTime(data) {
-    const { seconds } = data;
-    localStorage.setItem('videoplayer-current-time', seconds);
-    return seconds;
+  const { seconds } = data;
+  localStorage.setItem('videoplayer-current-time', seconds);
+  return seconds;
 }
 
 function setTime() {
-    const currentTime = JSON.parset(localStorage.getItem('videoplayer-current-time'));
-    player.setCurrentTime(currentTime);
+  const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(currentTime);
 }
 
 player.on('timeupdate', throttle(saveTime, 1000, { leading: false }));
